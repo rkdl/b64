93b931393dc8a92d56b6b0525f59b91c1b8e79c4
Bogdan Minzar <bogdan.minzar@gmail.com>
2019-03-16 19:58:32 +0200

    added stubs

---
+++ base/base_velosiped.py
@@ -0,0 +1,15 @@
+ALPHABET = bytes(range(64))
+CHUNK_BITS = 6
+GCD = 3
+NEWLINE_AFTER = 76
+
+
+# 000110 00   0111 0001   01 110001   011000 11
+
+
+def to_base64(raw_input: bytes) -> bytes:
+    ...
+
+
+def base64_decode(raw_input: bytes) -> bytes:
+    ...
---
+++ base/tests.py
@@ -0,0 +1,20 @@
+import base64
+
+from .base_velosiped import base64_decode, to_base64
+
+
+def test_encode_latin():
+    text_data = 'vodka'
+    bts = bytes(text_data, 'utf8')
+    assert to_base64(bts) == base64.b64encode(bts)
+
+
+def test_encode_cyr():
+    text_data = 'Почему гугл не работает'
+    bts = bytes(text_data, 'utf8')
+    assert to_base64(bts) == base64.b64encode(bts)
+
+
+def test_decode():
+    encoded = b'ABYsSSSs'
+    assert base64_decode(encoded) == base64.b64decode(encoded)
